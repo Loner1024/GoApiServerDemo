694556f4c6fa7b5101b4c84b2d5397ccba4a0137
Loner1024 <xth12138@gmail.com>
2020-03-31 21:20:50 +0800

    用户逻辑处理

---
+++ handler/user/delete.go
@@ -0,0 +1,19 @@
+package user
+
+import (
+	. "main/handler"
+	"main/model"
+	"main/pkg/errno"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Delete(c *gin.Context) {
+	userId, _ := strconv.Atoi(c.Param("id"))
+	if err := model.DeleteUser(uint64(userId)); err != nil {
+		SendResponse(c, errno.ErrDatabase, nil)
+		return
+	}
+	SendResponse(c, nil, nil)
+}
