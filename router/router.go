ce5b5193650c19d21a1541cbc81173e6643cf504
Loner1024 <xth12138@gmail.com>
2020-04-01 21:23:40 +0800

    API 身份验证,close #8

--- router/router.go
+++ router/router.go
@@ -18,12 +18,16 @@ func Load(g *gin.Engine, mw ...gin.HandlerFunc) *gin.Engine {
 	g.Use(middleware.Secure)  // 安全设置
 	g.Use(mw...)
 
+	// api for authentication functionalities
+	g.POST("/login", user.Login)
+
 	// 404 NoRoute 默认返回 404
 	g.NoRoute(func(c *gin.Context) {
 		c.String(http.StatusNotFound, "The incorrect API route.")
 	})
 
 	u := g.Group("/v1/user")
+	u.Use(middleware.AuthMiddleware())
 	{
 		u.POST("", user.Create)       // 创建用户
 		u.DELETE("/:id", user.Delete) // 删除用户
